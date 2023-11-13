202b7caa4e84de968946aecbc65e017b86a81242
LeNguyenTatThang <lenguyentatthang.dev@gmail.com>
2023-11-13 17:56:30 +0700

    add-img

--- Web-app-Thue_sach/Reactjs/src/Service/UserService.js
+++ Web-app-Thue_sach/Reactjs/src/Service/UserService.js
@@ -9,5 +9,8 @@ const loginApi = (email, matkhau) => {
 const apiRegister = (ten, email, matkhau,) => {
     return axios.post("/api-userRegister", { ten, email, matkhau });
 }
+const addBook = (hinh, ten, loai, theloai_id, gia, tiencoc, tentacgia, id_users) => {
+    return axios.post("/post-api-book", { hinh, ten, loai, theloai_id, gia, tiencoc, tentacgia, id_users });
+}
 
-export { fetchAllUser, loginApi, apiRegister };
+export { fetchAllUser, loginApi, apiRegister, addBook };
--- Web-app-Thue_sach/Reactjs/src/view/AddProduct.js
+++ Web-app-Thue_sach/Reactjs/src/view/AddProduct.js
@@ -3,25 +3,17 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
+import { addBook } from '../Service/UserService';
 
 export default function AddProduct() {
 
     const navigate = useNavigate();
+
     const [book, setBook] = useState(
         {
             loai: "",
         }
     );
-    // const [catetory, setcateRoty] = useState([
-    //     {
-    //         label: 'Sách đọc miễn phí',
-    //         value: 1
-    //     },
-    //     {
-    //         label: 'Sách cho thuê',
-    //         value: 2
-    //     }
-    // ])
     const handleRead = (e) => {
         setBook(e.target.value);
     }
@@ -32,19 +24,85 @@ export default function AddProduct() {
         setBook(formData)
     }
 
-    function submitForm(e) {
-        console.log(e)
-    }
 
+    const [ten, setTen] = useState();
+    const [hinh, setHinh] = useState({ preview: '', data: '' });
+    const [gia, setGia] = useState();
+    const [tiencoc, setTiencoc] = useState();
+    const [tentacgia, setTentacgia] = useState();
+    const [theloai_id, setTheloai_id] = useState(1);
+    const [id_users, setId_user] = useState()
     const userData = useSelector((state) => state.user);
 
     useEffect(() => {
         if (!userData.isLogin) {
             navigate(-1);
         }
+        if (userData.isLogin) {
+            setId_user(userData.userInfo.id)
+        }
+
     }, [])
 
     const dispatch = useDispatch();
+
+    const handleChange = (e) => {
+        const img = {
+            preview: URL.createObjectURL(e.target.files[0]),
+            data: e.target.files[0],
+        }
+        setHinh(img)
+
+    }
+
+    const handleSend = async (e) => {
+        // if (book.loai === '1') {
+        //     if (!ten || !gia || !tiencoc || !tentacgia) {
+        //         iziToast.error({
+        //             title: "Opzzz!!",
+        //             position: "topRight",
+        //             message: "khong duoc de trong"
+        //         })
+        //     }
+        // }
+        // else if (book.loai === '0') {
+        //     if (!tentacgia) {
+        //         iziToast.error({
+        //             title: "Opzzz!!",
+        //             position: "topRight",
+        //             message: "khong duoc de trong"
+        //         })
+        //     }
+        // }
+        // if (!ten || !gia || !tiencoc || !tentacgia) {
+        //     iziToast.error({
+        //         title: "Opzzz!!",
+        //         position: "topRight",
+        //         message: "khong duoc de trong"
+        //     })
+        // }
+        // if (book.loai === '') {
+        //     iziToast.error({
+        //         title: "Opzzz!!",
+        //         position: "topRight",
+        //         message: "Hay chon the loai sach dang"
+        //     })
+        // }
+        const formData = new FormData()
+        formData.append('file', hinh.data)
+
+
+        let res = await addBook(formData, ten, book.loai, theloai_id, gia, tiencoc, tentacgia, id_users);
+
+        console.log("check res:", res);
+        if (res && res.status === 200) {
+            iziToast.success({
+                title: "Succes",
+                position: "topRight",
+                message: res.message
+            })
+        }
+    }
 
     return (
         <div>
@@ -60,12 +118,15 @@ export default function AddProduct() {
                             <div className="space-y-4 md:space-y-6">
                                 <div>
                                     <label className="block mb-2 text-sm font-medium text-gray-900 ">Tên sách</label>
-                                    <input type="text" name="tensach" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="" />
+                                    <input type="text" name="ten" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required=""
+                                        value={ten}
+                                        onChange={(event) => { setTen(event.target.value) }} />
                                 </div>
                                 <select onChange={(e) => handleRead1(e)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 placeholder='thuesach@gmail.com' ">
                                     <option  >Chon loai sach</option>
-                                    <option value={0} >Sách đọc miễn phí</option>
-                                    <option value={1}>Sách cho thuê</option>
+
+                                    <option value={0}>Sách cho thuê</option>
+                                    <option value={1} >Sách đọc miễn phí</option>
                                 </select>
 
                                 <select onChange={(e) => handleRead(e)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ">
@@ -74,44 +135,43 @@ export default function AddProduct() {
                                 </select>
                                 <div>
                                     <label className="block mb-2 text-sm font-medium text-gray-900 ">Hình ảnh</label>
-                                    <input type="file" className="file-input w-full max-w-xs" />
+                                    <input type="file" name='hinh' className="file-input w-full max-w-xs"
+                                        onChange={handleChange} />
+                                    <img src={hinh.preview} />
+                                </div>
+                                <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 ">Tên tác giả</label>
+                                    <input type="text" name="tentacgia" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required=""
+                                        value={tentacgia}
+                                        onChange={(event) => { setTentacgia(event.target.value) }} />
                                 </div>
 
-                                {book.loai === '1' ?
+
+                                {book.loai === '0' ?
                                     <>
                                         <div>
                                             <label className="block mb-2 text-sm font-medium text-gray-900 ">Giá tiền</label>
-                                            <input type="number" name="giatien" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="" />
+                                            <input type="number" name="gia" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required=""
+                                                value={gia}
+                                                onChange={(event) => { setGia(event.target.value) }} />
                                         </div>
                                         <div>
                                             <label className="block mb-2 text-sm font-medium text-gray-900 ">Tiền cọc</label>
-                                            <input type="number" name="tiencoc" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
+                                            <input type="number" name="tiencoc" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required=""
+                                                value={tiencoc}
+                                                onChange={(event) => { setTiencoc(event.target.value) }} />
                                         </div>
-                                        <div>
-                                            <label className="block mb-2 text-sm font-medium text-gray-900 ">Tên tác giả</label>
-                                            <input type="text" name="tentacgia" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="" />
-                                        </div>
+
 
                                     </>
                                     :
                                     <>
 
-                                        <div>
-                                            <label className="block mb-2 text-sm font-medium text-gray-900 ">Tên tác giả</label>
-                                            <input type="text" name="tentacgia" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="" />
-                                        </div>
+
                                     </>
                                 }
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 " required="" />
-                                    </div>
-                                    <div className="ml-3 text-sm">
-                                        <label for="terms" className="font-light text-gray-500 ">Tôi đồng ý <div className="font-medium text-primary-600 hover:underline " href="#">điều khoản và chính sách</div></label>
-                                    </div>
-                                </div>
-                                <button type="submit" className="w-full text-black bg-slate-600 hover:bg-red-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center">Send</button>
-
+                                <button type="submit" className="w-full text-black bg-slate-600 hover:bg-red-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center"
+                                    onClick={() => handleSend()}>Send</button>
                             </div>
                         </div>
                     </div>
