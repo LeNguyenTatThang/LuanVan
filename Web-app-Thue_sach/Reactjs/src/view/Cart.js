5b22b950342eb4b5ba9f4c6a84a9fef5d7fecdfb
LeNguyenTatThang <lenguyentatthang.dev@gmail.com>
2023-11-19 23:34:55 +0700

    add/del_itemCart

--- Web-app-Thue_sach/Reactjs/src/view/Cart.js
+++ Web-app-Thue_sach/Reactjs/src/view/Cart.js
@@ -3,12 +3,13 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export default function Cart() {
-    const [count, setCount ]= useState()
+    const [count, setCount] = useState()
     const card = useSelector((state) => state.shop.cart);
     // const a = card.length
     // count = a
     // setCount(a)
     // console.log(a)
+
     return (
         <>
 
@@ -17,15 +18,15 @@ export default function Cart() {
                     <label tabindex="0" className="btn btn-ghost btn-circle">
                         <div className="indicator">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                            <span className="badge badge-sm indicator-item">{card.length}</span>
+                            <div className=" badge badge-sm indicator-item left-3"><span className='absolute text-[14px] '>{card.length}</span></div>
                         </div>
                     </label>
                     <div tabindex="0" className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
                         <div className="card-body">
-                            <span className="font-bold text-lg">{card.length} Items</span>
-                            <span className="text-info">Subtotal: $999</span>
+                            <span className="font-bold text-lg">{card.length} Sản phẩm</span>
+                            <span className="text-info">Tổng: 999 vnđ</span>
                             <div className="card-actions">
-                                <Link to="/customer-card"><button className="btn btn-primary btn-block">View cart</button></Link>
+                                <Link to="/customer-card"><button className="btn btn-primary btn-block">Xem giỏ hàng</button></Link>
                             </div>
                         </div>
                     </div>
--- Web-app-Thue_sach/Reactjs/src/view/CustomerCard.js
+++ Web-app-Thue_sach/Reactjs/src/view/CustomerCard.js
@@ -1,48 +1,103 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { REMOVE_FROM_CART } from '../app/userCard';
 
 export default function CustomerCard() {
+
     const card = useSelector((state) => state.shop.cart);
-    console.log(card)
+
+    let TotalCart = 0;
+    Object.keys(card).forEach(function (item) {
+        TotalCart += card[item].gia;
+
+    });
+
+    console.log(">>>check card: ", card)
+
+    const userData = useSelector((state) => state.user);
+
+    const dispatch = useDispatch();
+
+    const DeleteCart = (_value) => {
+        dispatch(REMOVE_FROM_CART(_value));
+    }
+
     return (
         <>
-            <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                <div className="flex items-center justify-between mb-4">
-                    <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
-                    <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-                        View all
-                    </a>
+            <div className=' flex w-full'>
+                <div className="w-3/5 p-3 bg-white border border-gray-200 rounded-lg shadow ">
+                    <div className="flex items-center justify-between mb-4">
+                        <h5 className="text-xl font-bold leading-none text-gray-900 ">Giỏ hàng của bạn</h5>
+
+                    </div>
+                    {card.map((value, index) => {
+                        return (
+                            <React.Fragment key={index}>
+                                <div className="flow-root">
+                                    <ul role="list" className="divide-y divide-gray-200 ">
+                                        <li className="py-3 sm:py-4">
+                                            <div className="flex items-center">
+                                                <div className="flex-shrink-0">
+                                                    <img className="w-8 h-8 rounded-full" src={`http://localhost:8000/img/${value.hinh}`}
+                                                        alt={`${value.hinh}`} />
+                                                </div>
+                                                <div className="flex-1 min-w-0 ms-4">
+                                                    <p className="text-sm font-medium text-gray-900 truncate ">
+                                                        {value.ten}
+                                                    </p>
+                                                    <p className="text-sm text-gray-900 truncate">
+                                                        {value.tentacgia}
+                                                    </p>
+                                                </div>
+                                                <div className="inline-flex items-center text-base font-semibold text-gray-900 px-3">
+                                                    {value.gia} vnđ
+                                                </div>
+                                                <i className="badge badge-danger px-3 cursor-pointer" onClick={(e) => DeleteCart(value)}>X</i>
+                                            </div>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </React.Fragment>
+                        )
+                    })}
                 </div>
-                {card.map((value, index) => {
-                    return (
-                        <React.Fragment key={index}>
-                            <div className="flow-root">
-                                <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
-                                    <li className="py-3 sm:py-4">
-                                        <div className="flex items-center">
-                                            <div className="flex-shrink-0">
-                                                <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image" />
-                                            </div>
-                                            <div className="flex-1 min-w-0 ms-4">
-                                                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                    {value.ten}
-                                                </p>
-                                                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                    email@windster.com
-                                                </p>
-                                            </div>
-                                            <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                                $320
-                                            </div>
-                                        </div>
-                                    </li>
+                <div className="w-2/5 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8">
+                    <div className="flex items-center justify-between mb-4">
+                        <h5 className="text-xl font-bold leading-none text-gray-900 ">Thong tin ca nhan</h5>
 
-                                </ul>
-                            </div>
-                        </React.Fragment>
-                    )
-                })}
+                    </div>
 
+                    <div className="flow-root">
+                        <ul role="list" className="divide-y divide-gray-200 ">
+                            <li className="py-3 sm:py-4">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0">
+                                        <img className="w-8 h-8 rounded-full" src={`http://localhost:8000/img/${userData.hinh}`}
+                                            alt={`${userData.hinh}`} />
+                                    </div>
+                                    <div className=" text-center w-full">
+                                        <p className="text-sm font-medium text-gray-900 truncate ">
+                                            {userData.userInfo.ten}
+                                        </p>
+                                        <p className="text-sm text-gray-900  truncate">
+                                            {userData.userInfo.email}
+                                        </p>
+                                        <p className="text-sm text-gray-900  truncate">
+                                            dia chi
+                                        </p>
+                                        <p className="text-sm text-gray-900  truncate">
+                                            sdt
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <h3 className="text-xl leading-none text-gray-900 ">Tong tien: {TotalCart} vnđ</h3>
+                </div>
             </div>
         </>
     )
